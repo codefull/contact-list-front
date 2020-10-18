0bdf5900f0a5a420578d7b3817658c8a1fb47798
Ernesto Guevara Ramírez <eguevarar@gmail.com>
2020-10-18 15:54:44 -0400

    Change lib client for fetching results

--- src/Redux/actions/fetchContacts.ts
+++ src/Redux/actions/fetchContacts.ts
@@ -3,6 +3,7 @@ import {
   fetchContactsSuccess,
   fetchContactsError,
 } from './contactActions'
+import axios from 'axios'
 
 //TODO: Modify backend to take intoo account error results
 /**
@@ -11,18 +12,32 @@ import {
 export const fetchContacts = () => {
     return (dispatch: any) => {
         dispatch(requestContacts())
-    return fetch(`localhost:3000/contact`)
-      .then(response => response.json())
-      .then(res => {
-        if (res.error) {
-          throw res.error
-        }
-        dispatch(fetchContactsSuccess(res.contacts))
-        return res.contacts
-      })
-      .catch(error => {
-        dispatch(fetchContactsError(error.message))
-      })
+    // return fetch(`http://localhost:3000/contact`)
+    //   .then(response => response.json())
+    //   .then(res => {
+    //     if (res.error) {
+    //       throw res.error
+    //     }
+    //     dispatch(fetchContactsSuccess(res.contacts))
+    //     return res.contacts
+    //   })
+    //   .catch(error => {
+    //     dispatch(fetchContactsError(error.message))
+    //   })
+    return axios
+    .get('http://localhost:3000/contact')
+    .then(res => {
+      const {error} = res.data
+      if (error) {
+        throw error
+      }
+      const contacts = res.data.data
+      dispatch(fetchContactsSuccess(contacts))
+      return contacts;
+    })
+    .catch(error => {
+      dispatch(fetchContactsError(error.message))
+    })
   }
 }
 
