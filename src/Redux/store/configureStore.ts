cb34a5df836498b28b5f553bcbd83ac939fbd509
Ernesto Guevara Ramírez <eguevarar@gmail.com>
2020-10-18 04:13:14 -0400

    Configure Store refactoring

--- src/Redux/store/configureStore.ts
+++ src/Redux/store/configureStore.ts
@@ -1,8 +1,17 @@
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const configureStore = (initialState: any = []) => {
-  return createStore(rootReducer, initialState)
+
+
+const _initialState = {
+  pending: false,
+  contacts: [],
+  error: ""
+}
+
+const configureStore = (initialState: any = _initialState) => {
+  return createStore(rootReducer, initialState, applyMiddleware(thunk))
 }
 
 export default configureStore
