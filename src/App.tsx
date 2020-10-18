4a182a77ba3e34db91faab4022c63374619ff32e
Ernesto Guevara Ramírez <eguevarar@gmail.com>
2020-10-18 04:15:39 -0400

    Added Contact List component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import ContactList from './Containers/ContactList'
 import './App.css';
+//import { useSelector, useDispatch} from 'react-redux'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Lista de contactos</p>
+        <ContactList/>
       </header>
     </div>
   );
