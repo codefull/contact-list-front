f66b4bf0e8e5cadd537ee61b1f7eb1d93d72ce9a
Ernesto Guevara Ramírez <eguevarar@gmail.com>
2020-10-18 02:43:14 -0400

    Implementation of basic logic for contact fetching

--- src/Redux/reducers/contactReducer.ts
+++ src/Redux/reducers/contactReducer.ts
@@ -1,14 +1,35 @@
 import * as actionTypes from '../actions/ActionTypes';
 
-export default (state = [], action:any) => {
-    switch (action.type) {
-      
-      case actionTypes.SAVE_CONTACT:
-      return [
-        ...state,
-        Object.assign({}, action.contact)
-      ];
-      default:
-            return state;
-    }
-  };
+const initialState = {
+  pending: false,
+  contacts: [],
+  error: ""
+}
+
+export function contactsReducer(state = initialState, action:any) {
+  switch(action.type) {
+      case actionTypes.REQUEST_CONTACTS: 
+          return {
+              ...state,
+              pending: true
+          }
+      case actionTypes.FETCH_CONTACTS_SUCCESS:
+          return {
+              ...state,
+              pending: false,
+              contacts: action.contacts
+          }
+      case actionTypes.FETCH_CONTACTS_ERROR:
+          return {
+              ...state,
+              pending: false,
+              error: action.error
+          }
+      default: 
+          return state;
+  }
+}
+
+export const getContacts = (state:any) => state.contacts;
+export const getContactsPending = (state:any) => state.pending;
+export const getContactsError = (state:any) => state.error;
