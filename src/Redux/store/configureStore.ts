144a7312a28306a1afae3982373fea32e769014b
Ernesto Guevara Ramírez <eguevarar@gmail.com>
2020-10-18 00:46:52 -0400

    Initial Store configuration functionality

--- src/Redux/store/configureStore.ts
+++ src/Redux/store/configureStore.ts
@@ -1,7 +1,8 @@
 import { createStore } from 'redux'
+import rootReducer from '../reducers';
 
-const configureStore = (initialState: any) => {
-  return createStore(initialState)
+const configureStore = (initialState: any = []) => {
+  return createStore(rootReducer, initialState)
 }
 
 export default configureStore
