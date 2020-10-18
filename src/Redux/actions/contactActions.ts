16503d182d3b46f2eff139b127b1df0c4f07e255
Ernesto Guevara Ramírez <eguevarar@gmail.com>
2020-10-18 02:40:07 -0400

    Implemented actions for contact fetching

--- src/Redux/actions/contactActions.ts
+++ src/Redux/actions/contactActions.ts
@@ -1,22 +1,45 @@
-import * as actionTypes from './ActionTypes';
+import * as actionTypes from './ActionTypes'
 
-export const editContact = (contact:any) => {
-    return {
-      type: actionTypes.EDIT_CONTACT,
-      contact: contact
-    }
-  };
+/**
+ * @summary Action dispatched when a request for contacts loading is initiated.
+ */
+export const requestContacts = () => {
+  return {
+    type: actionTypes.REQUEST_CONTACTS,
+  }
+}
 
-  export const saveContact = (contact:any) => {
-    return {
-      type: actionTypes.SAVE_CONTACT,
-      contact: contact
-    }
-  };
+export const fetchContactsSuccess = (contacts: any) => {
+  return {
+    type: actionTypes.FETCH_CONTACTS_SUCCESS,
+    contacts,
+  }
+}
 
-  export const removeContact = (contact:any) => {
-    return {
-      type: actionTypes.REMOVE_CONTACT,
-      contact: contact
-    }
-  };
+export const fetchContactsError = (error:string) => {
+  return {
+    type: actionTypes.FETCH_CONTACTS_ERROR,
+     error
+}
+}
+
+export const editContact = (contact: any) => {
+  return {
+    type: actionTypes.EDIT_CONTACT,
+    contact: contact,
+  }
+}
+
+export const saveContact = (contact: any) => {
+  return {
+    type: actionTypes.SAVE_CONTACT,
+    contact: contact,
+  }
+}
+
+export const removeContact = (contact: any) => {
+  return {
+    type: actionTypes.REMOVE_CONTACT,
+    contact: contact,
+  }
+}
